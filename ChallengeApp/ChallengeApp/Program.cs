429b97adb9dcb481411934ecd46f2f736ff4722c
kondzikslaw <kondzikslaw@gmail.com>
2023-04-14 13:05:25 +0200

    Challenge Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Konrad";
+char sex = 'm';
+int age = 28;
+
+if (age < 30 && sex == 'm')
+{
+    Console.WriteLine("Mężczyzna poniżej 30 lat");
+}
+else if (name == "Konrad" && age == 28)
+{
+    Console.WriteLine("Konrad, lat 28");
+}
+else if (sex == 'm' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
