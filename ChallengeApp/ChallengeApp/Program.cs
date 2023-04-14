686261ba9471c53d9b437f7bf51801efa0a264ba
kondzikslaw <kondzikslaw@gmail.com>
2023-04-14 20:15:30 +0200

    Challenge dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,16 +1,59 @@
-﻿string name = "Konrad";
-char sex = 'm';
-int age = 28;
+﻿int number = 3744;
+string numberInString = number.ToString();
+char[] numbers = numberInString.ToArray();
 
-if (age < 30 && sex == 'm')
+int[] counters = new int[10];
+
+for (int i = 0; i < counters.Length; i++)
 {
-    Console.WriteLine("Mężczyzna poniżej 30 lat");
+    counters[i] = 0;
 }
-else if (name == "Konrad" && age == 28)
+
+foreach (char c in numbers)
 {
-    Console.WriteLine("Konrad, lat 28");
+    if (c == '0')
+    {
+        counters[0]++;
+    }
+    else if (c == '1')
+    {
+        counters[1]++;
+    }
+    else if (c == '2')
+    {
+        counters[2]++;
+    }
+    else if (c == '3')
+    {
+        counters[3]++;
+    }
+    else if (c == '4')
+    {
+        counters[4]++;
+    }
+    else if (c == '5')
+    {
+        counters[5]++;
+    }
+    else if (c == '6')
+    {
+        counters[6]++;
+    }
+    else if (c == '7')
+    {
+        counters[7]++;
+    }
+    else if (c == '8')
+    {
+        counters[8]++;
+    }
+    else if (c == '9')
+    {
+        counters[9]++;
+    }
 }
-else if (sex == 'm' && age < 18)
+
+for (int i = 0; i < counters.Length; i++)
 {
-    Console.WriteLine("Niepełnoletni Mężczyzna");
+    Console.WriteLine(i + "=>" +counters[i]);
 }
